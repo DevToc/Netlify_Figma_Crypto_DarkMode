dfaf3c2ef857b82a6291f199df9d8f1d930191e5
SecretDevStar <ztoc0130@gmail.com>
2022-11-21 17:54:43 +0900

    aa

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,16 @@
 toggleTheme = () => {
     document.body.classList.toggle("light")
 }
+
+showAddress = () => {
+    if(window.innerWidth>=600){
+        return;
+    }
+    if(document.getElementById('wallet').style.display=='block'){
+        document.getElementById('wallet').style.display='none'
+    }
+    else {
+        document.getElementById('wallet').style.display='block'
+
+    }
+}
